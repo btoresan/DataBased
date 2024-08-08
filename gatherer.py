decb9cb7b212901b30a5989e21a45aff01239d87
Nickyecen <nicolasecendron@gmail.com>
2024-08-08 18:28:08 -0300

    Added steam scraper

--- gatherer.py
+++ gatherer.py
@@ -8,30 +8,56 @@ from datetime import datetime
 
 import asyncio
 
-async def gather_protondb_async(urls, batch_size):
+async def gather_protondb_async(urls, num_windows, func):
+    batch_size = len(urls) // num_windows + 1
     batches = [urls[i:i+batch_size] for i in range(0, len(urls), batch_size)] 
     tasks = []
 
     for batch in batches:
-        task = asyncio.to_thread(gather_protondb, batch)
+        task = asyncio.to_thread(gatherer, batch, func)
         tasks.append(task)
 
     await asyncio.gather(*tasks)
         
-def gather_protondb(urls):
+def gatherer(urls, func):
     gecko_path = '/home/nickyecen/.wdm/drivers/geckodriver/linux64/v0.35.0/geckodriver'
     driver = webdriver.Firefox(service=Service(gecko_path))
-    data = [scrape_protondb(driver, url) for url in urls]
+    data = [func(driver, url) for url in urls]
     driver.quit()
 
     return data
+
+def scrape_steam(driver, url):
+    driver.get(url)
+
+    try:
+        wait = WebDriverWait(driver, 5)
+
+        developer = wait.until(EC.presence_of_element_located((By.XPATH, '//a[@itemprop="author"]'))).text
+        print(developer)
+
+        release = wait.until(EC.presence_of_element_located((By.XPATH, '//tr[td[text()="Release Date"]]'))).text
+        release_list = release.split()
+        release = release_list[2]+' '+release_list[3]+' '+release_list[4]+' '+release_list[6]
+        time_format = '%d %B %Y %H:%M:%S'
+        release = int(datetime.strptime(release, time_format).timestamp())
+
+        print(release)
+   
+        return {
+                'id': url.split('/')[-1],
+                'developer': developer,
+                'release': release
+                }
+
+    except Exception as e:
+        print(url.split('/')[-1], e)
 
 def scrape_protondb(driver, url):
     driver.get(url)
 
     try:
-        # Wait for the element to be present for up to 10 seconds
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 30)
 
         title_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[class^='GameInfo__']")))
         title = title_element.text
@@ -59,12 +85,13 @@ def scrape_protondb(driver, url):
         print(url.split('/')[-1], e)
 
 # Example usage:
-t = ['https://www.protondb.com/app/240', 'https://www.protondb.com/app/550', 'https://www.protondb.com/app/444090',
+t0 = ['https://steamdb.info/app/1119730/']
+t1 = ['https://www.protondb.com/app/240', 'https://www.protondb.com/app/550', 'https://www.protondb.com/app/444090',
                 'https://www.protondb.com/app/387990', 'https://www.protondb.com/app/227940', 'https://www.protondb.com/app/460930',
                 'https://www.protondb.com/app/379720', 'https://www.protondb.com/app/440', 'https://www.protondb.com/app/221100']
-t *= 10
+#t *= 10
 
 init = datetime.now()
-asyncio.run(gather_protondb_async(t, 25))
+asyncio.run(gather_protondb_async(t0, 4, scrape_steam))
 print(datetime.now() - init)
 
